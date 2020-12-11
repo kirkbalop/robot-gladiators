8b330b1ca19cd31fca99c3bc4f330e644c99dcc8
Kirk <kirkbalop@gmail.com>
2020-12-11 13:15:13 -0800

    shortened input requirements

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -106,24 +106,21 @@ var endGame = function() {
 
 var shop = function() {
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL,' 'UPGRADE,' or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: '1' to refill, '2' to upgrade, or '3' to leave."
     );
+
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+
         switch(shopOptionPrompt) {
-            case "refill":
-            case "REFILL":
-            case "Refill":
+            case 1:
                 playerInfo.refillHealth();
                 break; 
 
-            case "upgrade":
-            case "UPGRADE":
-            case "Upgrade":
+            case 2:
                 playerInfo.upgradeAttack();
                 break;
 
-            case "leave":
-            case "LEAVE":
-            case "Leave":
+            case 3:
                 window.alert("Leaving the store.");
                 break;
 
